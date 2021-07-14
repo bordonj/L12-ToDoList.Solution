5ec990bfd098c73ac9e609a2771460dfbb05ec73
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-07-14 16:28:33 -0700

    add cool console menu
    Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>
    Co-Authored-By: Allister <allisterkmoon@gmail.com>

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -6,27 +6,67 @@ namespace ToDoList
 {
   public class Program
   {
+    private static int selectedOption = 0;
+
     public static void Main()
     {
-      Console.WriteLine("Hello");
+      Console.Clear();
       Console.WriteLine("Welcome to the To Do List!!!");
-      Console.WriteLine("Would you like to add an Item, or view your List?! (add/view)");
-      string userAnswer = Console.ReadLine();
-      if (userAnswer == "add") 
-      {
-        AddItem();
+      // Console.WriteLine("Would you like to add an Item, or view your List?! (add/view)");
+
+      if (selectedOption == 0) {
+        WriteLine("- add", ConsoleColor.White, ConsoleColor.Black);
+      } else {
+        Console.WriteLine("- add");
       }
-      else if (userAnswer == "view")
-        { 
-          ViewItems();
+
+      if (selectedOption == 1) {
+        WriteLine("- view", ConsoleColor.White, ConsoleColor.Black);
+      } else {
+        Console.WriteLine("- view");
+      }
+
+      while (true) {
+        ConsoleKeyInfo pressedKey = Console.ReadKey(true);
+
+        if (pressedKey.Key == ConsoleKey.UpArrow) {
+          selectedOption--;
+          Main();
         }
+
+        if (pressedKey.Key == ConsoleKey.DownArrow) {
+          selectedOption++;
+          Main();
+        }
+
+        if (pressedKey.Key == ConsoleKey.Enter) {
+          if (selectedOption == 0) {
+            AddItem();
+          } else if (selectedOption == 1) {
+            ViewItems();
+          } else {
+            Main();
+          }
+        }
+      }
     }
+
+    static void WriteLine (string text, ConsoleColor background, ConsoleColor foreground)
+    {
+        Console.BackgroundColor = background;
+        Console.ForegroundColor = foreground;
+        Console.WriteLine(text);
+        Console.ResetColor();
+    }
+
     static void AddItem()
     {
       Console.WriteLine("Please enter description of the Item you would like to add:");
       string newItem = Console.ReadLine();
       Item addedItem = new Item(newItem);
       Console.WriteLine($"{newItem} has been added to your list!!");
+
+      WaitForAnyKeypress();
       Main();
     }
 
@@ -34,7 +74,15 @@ namespace ToDoList
     {
       List<Item> result = Item.GetAll();
       result.ForEach((item) => Console.WriteLine(item.Description));
+
+      WaitForAnyKeypress();
       Main();
+    }
+
+    static void WaitForAnyKeypress()
+    {
+      Console.WriteLine("-- press any key to go back to main menu");
+      Console.ReadKey();
     }
   }
 }
