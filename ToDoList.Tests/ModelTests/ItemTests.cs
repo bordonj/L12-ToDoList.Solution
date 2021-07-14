d62772cff1295395074ed0cd456cc0eb9ddbabc5
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-07-14 01:42:22 -0700

    add second test and fix logic and og test to pass arg for constructor

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -10,8 +10,22 @@ namespace ToDoList.Tests// accidentally made this .Models instead of .Tests
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
-      Item newItem = new Item();
+      Item newItem = new Item("test");
       Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      // Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+
+      // Act
+      string result = newItem.Description;
+
+      // Assert
+      Assert.AreEqual(description, result);
     }
   }
 }
